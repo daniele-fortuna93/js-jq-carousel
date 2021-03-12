0e2752ae3b777a2b0ae0b82e7e17e43cfcf59ab7
Daniele <bambudio@libero.it>
2021-03-12 17:40:32 +0100

    Aggiunto click bullet

--- main.js
+++ main.js
@@ -1,34 +1,67 @@
 $(function() {
 
   $(".next").click(
-    function(){
-      var images = $(".images img.active");
-      var bluePoint = $(".nav i.active");
-      bluePoint.removeClass("active");
-      images.removeClass("active");
-      if ( images.hasClass("last") == false) {
-        images.next().addClass("active");
-        bluePoint.next().addClass("active");
-      } else {
-        $(".images img.first").addClass("active");
-        $(".nav i.first").addClass("active");
-      }
+    function() {
+      imgSuccessiva();
     }
   );
   $(".prev").click(
     function(){
-      var images = $(".images img.active");
-      var bluePoint = $(".nav i.active");
-      bluePoint.removeClass("active");
+      imgPrecedente();
+    }
+  );
+
+  $(".nav .fas.fa-circle").click(
+    function(){
+      var images = $(".images img");
+      var dots = $(".nav .fas.fa-circle");
+      var indexImg = dots.index($(this));
+      $(".nav").children().removeClass("active");
+      $(this).addClass("active");
       images.removeClass("active");
-      if ( images.hasClass("first") == false) {
-        images.prev().addClass("active");
-        bluePoint.prev().addClass("active");
-      } else {
-        $(".images img.last").addClass("active");
-        $(".nav i.last").addClass("active");
+      images.eq(indexImg).addClass("active");
+    }
+  );
+
+  $(document).keydown(
+    function (e) {
+      if ( e.keyCode == 39) {
+        imgSuccessiva();
+      } else if ( e.keyCode == 37 ) {
+        imgPrecedente();
       }
     }
   );
 }
 );
+
+// FUNZIONI
+
+// Funzione nextImg
+function imgSuccessiva() {
+  var images = $(".images img.active");
+  var bluePoint = $(".nav i.active");
+  bluePoint.removeClass("active");
+  images.removeClass("active");
+  if ( images.hasClass("last") == false) {
+    images.next().addClass("active");
+    bluePoint.next().addClass("active");
+  } else {
+    $(".images img.first").addClass("active");
+    $(".nav i.first").addClass("active");
+  }
+}
+
+function imgPrecedente() {
+  var images = $(".images img.active");
+  var bluePoint = $(".nav i.active");
+  bluePoint.removeClass("active");
+  images.removeClass("active");
+  if ( images.hasClass("first") == false) {
+    images.prev().addClass("active");
+    bluePoint.prev().addClass("active");
+  } else {
+    $(".images img.last").addClass("active");
+    $(".nav i.last").addClass("active");
+  }
+}
